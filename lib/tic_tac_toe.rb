977ab0d0c7fa6712ce886cc54ac7acc3c29a2785
Raymond Aranda <raymond.aranda@gmail.com>
2017-11-02 01:27:06 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -79,8 +79,8 @@ class TicTacToe
   end
 
   def winner
-  if winning_combo = won?(board)
-    @board[winning_combo.first]
+    if winning_combo = won?
+      @board[winning_combo.first]
+    end
   end
-end
 end
