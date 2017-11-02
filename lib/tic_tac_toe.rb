b4a7cbad491547fbb5b62d30e14a13a7f931b087
Raymond Aranda <raymond.aranda@gmail.com>
2017-11-02 01:15:41 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -46,4 +46,16 @@ class TicTacToe
   def current_player
     turn_count % 2 == 0 ? "X" : "O"
   end
+
+  def turn
+    puts "Please enter 1-9:"
+    user_input = gets.strip
+    index = input_to_index(user_input)
+    if valid_move?(index)
+      move(index, token)
+      display_board
+    else
+      turn
+    end
+  end
 end
